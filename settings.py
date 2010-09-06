56aa4f70fb361be2e8b0189fa4d2cb311b4c6e23
Ade Oshineye <adewale@gmail.com>
2010-09-06 05:03:12 +0100

    Fixing problems caused by making changes on the wrong branch

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 
 # These are the settings that tend to differ between installations of Streamer. Tweak these for your installation.
 
-APP_NAME = "twtweets"
+APP_NAME = "streamer-ade"
 
 # This is the token that will act as a shared secret to verify that this application is the one that registered the given subscription. The hub will send us a challenge containing this token.
 SECRET_TOKEN = "SOME_SECRET_TOKEN"
